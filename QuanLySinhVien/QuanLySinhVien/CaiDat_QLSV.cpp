7089830d8ab3479e4eb36b50849ee3aeba85c790
PhamLinhTam <phamlinhtam7114@gmail.com>
2019-11-25 22:19:42 +0700

    Chinh sua thu vien va cai dat

--- QuanLySinhVien/QuanLySinhVien/CaiDat_QLSV.cpp
+++ QuanLySinhVien/QuanLySinhVien/CaiDat_QLSV.cpp
@@ -1,1 +1,37 @@
 #include "ThuVien_QLSV.h"
+
+void nhap_SV(SINHVIEN &sv) {
+
+}
+
+void xuat_SV(SINHVIEN sv) {
+
+}
+
+void tinhDTB_SV(SINHVIEN &sv) {
+
+}
+
+void xeploai_SV(SINHVIEN a) {
+
+}
+
+void nhap_DSSV(DANHSACH_SV a[], int n) {
+
+}
+
+void xuat_DSSV(DANHSACH_SV a[], int n) {
+
+}
+
+void sapxep_DSSV(DANHSACH_SV a[], int n) {
+
+}
+
+void xeploai_DSSV(DANHSACH_SV a[], int n) {
+
+}
+
+void xuatFile(DANHSACH_SV a[], int n, char fileName[]) {
+
+}
--- QuanLySinhVien/QuanLySinhVien/ChuongTrinh_QLSV.cpp
+++ QuanLySinhVien/QuanLySinhVien/ChuongTrinh_QLSV.cpp
@@ -8,16 +8,17 @@ void main(){
 	bool daNhap = false;
 	while (true){
 		system("cls");
-		cout << ("******************************************\n");
-		cout << ("**    CHUONG TRINH QUAN LY SINH VIEN    **\n");
-		cout << ("**      1. Nhap du lieu                 **\n");
-		cout << ("**      2. In danh sach sinh vien       **\n");
-		cout << ("**      3. Sap xep sinh vien theo DTB   **\n");
-		cout << ("**      4. Xep loai sinh vien           **\n");
-		cout << ("**      5. Xuat DS sinh vien            **\n");
-		cout << ("**      0. Thoat                        **\n");
-		cout << ("******************************************\n");
-		cout << ("**       Nhap lua chon cua ban          **\n");
+		cout << "******************************************\n";
+		cout << "**    CHUONG TRINH QUAN LY SINH VIEN    **\n";
+		cout << "**      1. Nhap du lieu                 **\n";
+		cout << "**      2. In danh sach sinh vien       **\n";
+		cout << "**      3. Sap xep sinh vien theo DTB   **\n";
+		cout << "**      4. Xep loai sinh vien           **\n";
+		cout << "**      5. Xuat DS sinh vien            **\n";
+		cout << "**      0. Thoat                        **\n";
+		cout << "******************************************\n";
+		cout << "**       Nhap lua chon cua ban          **\n";
+		cin >> key;
 		switch (key){
 		case 1:
 			break;
